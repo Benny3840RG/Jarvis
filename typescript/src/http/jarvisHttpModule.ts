9fb7f5931e3626cc9ee3611de2c0addddf05acc4
Benny3840 <162340072+Benny3840@users.noreply.github.com>
2026-07-15 01:54:11 +1000

    feat: register Totality HTTP pipeline

--- typescript/src/http/jarvisHttpModule.ts
+++ typescript/src/http/jarvisHttpModule.ts
@@ -4,18 +4,26 @@ import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 
 import type { PersistenceProvider } from "../persistence/persistence.js";
 import type { PersistenceProviderName } from "../persistence/providerSelection.js";
+import type { TotalityPipeline } from "../totality/totalityPipeline.js";
 import type { HttpAppConfig } from "./config.js";
 import { ProblemDetailsFilter } from "./problemDetails.js";
 import { RequestIdInterceptor } from "./requestId.js";
 import { ServiceTokenGuard } from "./serviceTokenGuard.js";
 import { HealthController, OperatorSystemController } from "./systemControllers.js";
 import { SystemStatusService } from "./systemStatusService.js";
-import { HTTP_APP_CONFIG, HTTP_PERSISTENCE, HTTP_PROVIDER_NAME } from "./tokens.js";
+import { TotalityController } from "./totalityController.js";
+import {
+  HTTP_APP_CONFIG,
+  HTTP_PERSISTENCE,
+  HTTP_PROVIDER_NAME,
+  HTTP_TOTALITY_PIPELINE,
+} from "./tokens.js";
 
 export type JarvisHttpModuleOptions = {
   persistence: PersistenceProvider;
   providerName: PersistenceProviderName;
   config: HttpAppConfig;
+  totalityPipeline: TotalityPipeline | null;
 };
 
 @Module({})
@@ -23,11 +31,12 @@ export class JarvisHttpModule {
   static register(options: JarvisHttpModuleOptions): DynamicModule {
     return {
       module: JarvisHttpModule,
-      controllers: [HealthController, OperatorSystemController],
+      controllers: [HealthController, OperatorSystemController, TotalityController],
       providers: [
         { provide: HTTP_APP_CONFIG, useValue: options.config },
         { provide: HTTP_PERSISTENCE, useValue: options.persistence },
         { provide: HTTP_PROVIDER_NAME, useValue: options.providerName },
+        { provide: HTTP_TOTALITY_PIPELINE, useValue: options.totalityPipeline },
         SystemStatusService,
         { provide: APP_GUARD, useClass: ServiceTokenGuard },
         { provide: APP_INTERCEPTOR, useClass: RequestIdInterceptor },
